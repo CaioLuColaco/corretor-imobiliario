1a8cfb1678dc028b9ce3822361aeed8b4490d6e4
caio lucena <caio.lcolaco@gmail.com>
2022-09-05 21:57:10 -0300

    Adicionando rota para tela do administrador

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const { routerLogin } = require('./controller/routes/Login')
 const { routerFinancas } = require('./controller/routes/Financas')
 const { routerImovel } = require('./controller/routes/imovel')
 const { routerImoveis } = require('./controller/routes/imoveis')
+const { routerIhone } = require('./controller/routes/admin')
 
 app.set('view engine', 'ejs');
 app.use(express.static('views/static'));
 
-app.use('/', routerHome, routerLogin, routerFinancas, routerImovel, routerImoveis)
+app.use('/', routerHome, routerLogin, routerFinancas, routerImovel, routerImoveis, routerIhone)
 
 app.listen(serverConfig.port, () => {
     console.log("SERVER IS RUNNING!")
