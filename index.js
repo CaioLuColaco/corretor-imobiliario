ac3f35860c84a138dbd82dc721f0f2adf1e4a095
caio lucena <caio.lcolaco@gmail.com>
2023-01-10 10:08:09 -0300

    review index

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const routes = require('./controller/Routes')
 
 app.set('view engine', 'ejs');
 app.use(express.static('views/static'));
-app.use(express.json())
 app.use(express.json({limit: '50mb', extended:true}));
 app.use(express.urlencoded({extended: true, limit: '50mb', parameterLimit:10000000}));
 app.use(express.text({limit: '200mb'}));
