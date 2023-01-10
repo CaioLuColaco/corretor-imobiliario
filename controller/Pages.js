36d469d574a29ba30b6ec0d784c5781ddfd16a01
caio lucena <caio.lcolaco@gmail.com>
2023-01-10 09:54:49 -0300

    review pages controller

--- controller/Pages.js
+++ controller/Pages.js
@@ -70,7 +70,6 @@ module.exports = {
                 }
             })
 
-            //console.log(imoveis);    
             return imoveis? res.render("./screens/admin", {imoveis: imoveis}) : res.render("./screens/pageNotFind")
             
         } catch (error) {
