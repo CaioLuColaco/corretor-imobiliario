df9aecbcc75821f3eed3d6ab5ae0bc3fbf73aa9c
caio lucena <caio.lcolaco@gmail.com>
2023-01-25 15:59:11 -0300

    added animation with js to forms

---
+++ views/static/script/login.js
@@ -0,0 +1,16 @@
+
+// Animation to change the login to register
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+
+
+// Cadastro
